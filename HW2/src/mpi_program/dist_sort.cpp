b23cf16b3d3aca0f0429851c875e4c71542b58b4
Aadarsh Kenia <aadarsh.kenia@stonybrook.edu>
2016-04-06 02:21:59 -0400

    Completed Dist Sample sort using Insertion sort for local sorting.

--- HW2/src/mpi_program/dist_sort.cpp
+++ HW2/src/mpi_program/dist_sort.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <cmath>
 #include <cstdlib>
+#include <string.h>
 #include "../headers/array.h"
 
 #define MASTER_RANK 0
@@ -30,8 +31,7 @@ void insertion_sort(double * x, int start, int end){
 }
 
 
-long binary_search(double key, double *arr, long start, long end)
-{
+long binary_search(double key, double *arr, long start, long end){
 	long mid;
 	while(end >= start)
 	{
@@ -59,7 +59,6 @@ void print_array(double * x, long start, long end){
 	cout << endl;
 }
 
-
 void scatterKeysToNodes(double * x, long n, int numProcs){
 	long size_per_proc = n / (long)numProcs;	
 	long i = size_per_proc,j, processRank=1;
@@ -84,7 +83,7 @@ void getLocalPivotsFromSlaves(double * allPivots, double *x, long n, long q, int
 	long size_per_proc = n / (long)numProcs;
 	long allPivotPtr = 0;
 	long pivotsPerProc = size_per_proc/q;
-	long totalNumPivots = n/q;
+	long numLocalPivots = n/q;
 	long i;
 	//put pivots for MASTER in the array.
 	for(i = 0; i < size_per_proc; i++){
@@ -99,36 +98,34 @@ void getLocalPivotsFromSlaves(double * allPivots, double *x, long n, long q, int
 	}
 
 	// cout << "DISPLAYING ALL GATHERED LOCAL PIVOTS: " << endl;
-	// print_array(allPivots, 0, totalNumPivots - 1);
+	// print_array(allPivots, 0, numLocalPivots - 1);
 }
 
-
-void distributeGlobalPivotsToSlaves(double * allPivots, int numProcs, long numLocalPivots){
-	//Send p globally spaced pivots to all processes
-	int numGlobalPivots = numProcs-1;
-	long indexIncrement = numLocalPivots / (numProcs-1);
+void initializeGlobalPivots(double * allPivots, long numLocalPivots, double * globalPivArray, int numGlobalPivots){
+	long indexIncrement = numLocalPivots / numGlobalPivots;
 	long beginIndex = indexIncrement-1;
-	double * globalPivArray = new double[numGlobalPivots];
 	for(int i = 0; i < numGlobalPivots; i++){
 		globalPivArray[i] = allPivots[beginIndex];
 		beginIndex += indexIncrement;
 	}
+}
+
+
+double distributeGlobalPivotsToSlaves(double * globalPivArray, int numGlobalPivots, int numProcs){
+	//Send p globally spaced pivots to all processes
 
 	//print_array(globalPivArray, 0, numProcs-2);
-	for(int i=1; i <=numGlobalPivots ; i++){
-		MPI_Send(globalPivArray, numProcs-1, MPI_DOUBLE, i, DEFAULT_TAG, MPI_COMM_WORLD);
+	for(int i=1; i < numProcs ; i++){
+		MPI_Send(globalPivArray, numGlobalPivots, MPI_DOUBLE, i, DEFAULT_TAG, MPI_COMM_WORLD);
 	}
 }
 
-void sendBucketsToNodes(double * localKeys, int numKeys, double * globalPivots, int numProcs, int myrank){
+void sendBucketsToNodes(double * localKeys, long numKeys, double * globalPivots, int numProcs, int myrank, double * myBucket, long * myBucketPtr){
 	long numPivots = numProcs - 1;
 	long prevBucketEnd = -1;
 	long bucketNumber = -1;
 	for (int i = 0; i < numPivots; ++i)
 	{
-		if(myrank == i) //Bucket of current process remains with itself.
-			continue;
-
 		bucketNumber = i;
 		double pivot = globalPivots[i];
 		long pivotIndex = binary_search(pivot, localKeys, 0, numKeys-1);
@@ -148,8 +145,22 @@ void sendBucketsToNodes(double * localKeys, int numKeys, double * globalPivots, 
 		//Send numbers from index: prevBucketEnd+1 to newEndIndex to corresponding bucket.
 		long bucketSize = newEndIndex - prevBucketEnd;
 		long startIndex = prevBucketEnd + 1; 
-		if(bucketSize > 0 && startIndex >= 0 && startIndex < numKeys)
-			MPI_Send((void *)(localKeys + startIndex), bucketSize, MPI_DOUBLE, bucketNumber, DEFAULT_TAG, MPI_COMM_WORLD);
+		if(bucketSize > 0 && startIndex >= 0 && startIndex < numKeys){
+			if(bucketNumber != myrank){
+				MPI_Send((void *)&bucketSize, 1, MPI_LONG, bucketNumber, DEFAULT_TAG, MPI_COMM_WORLD);
+				MPI_Send((void *)(localKeys + startIndex), bucketSize, MPI_DOUBLE, bucketNumber, DEFAULT_TAG, MPI_COMM_WORLD);
+			}
+			else{
+				memcpy((void *)myBucket, (void *)(localKeys + startIndex), bucketSize*sizeof(double));
+				*myBucketPtr += bucketSize;
+			}
+		}
+		else{
+			if(bucketNumber != myrank){
+				long negative_size = -1;
+				MPI_Send((void *)&negative_size, 1, MPI_LONG, bucketNumber, DEFAULT_TAG, MPI_COMM_WORLD);
+			}
+		}
 
 		// if(myrank == 3 && bucketSize > 0 && startIndex >= 0 && startIndex < numKeys){
 		// 	cout << "For BUCKET: " << bucketNumber << endl;
@@ -162,11 +173,71 @@ void sendBucketsToNodes(double * localKeys, int numKeys, double * globalPivots, 
 	bucketNumber++;
 	long lastBucketStart = prevBucketEnd + 1;
 	long lastBucketSize = numKeys - lastBucketStart;
-	if(lastBucketSize > 0 && lastBucketStart >=0 && lastBucketStart < numKeys)
-		MPI_Send((void *)(localKeys + lastBucketStart), lastBucketSize, MPI_DOUBLE, bucketNumber, DEFAULT_TAG, MPI_COMM_WORLD);
-
+	if(lastBucketSize > 0 && lastBucketStart >=0 && lastBucketStart < numKeys){
+		if(bucketNumber != myrank){
+			MPI_Send((void *)&lastBucketSize, 1, MPI_LONG, bucketNumber, DEFAULT_TAG, MPI_COMM_WORLD);
+			MPI_Send((void *)(localKeys + lastBucketStart), lastBucketSize, MPI_DOUBLE, bucketNumber, DEFAULT_TAG, MPI_COMM_WORLD);
+		}
+		else{
+			memcpy((void *)myBucket, (void *)(localKeys + lastBucketStart), lastBucketSize*sizeof(double));
+			*myBucketPtr += lastBucketSize;
+		}
+	}
+	else{
+		if(bucketNumber != myrank){
+			long negative_size = -1;
+			MPI_Send((void *)&negative_size, 1, MPI_LONG, bucketNumber, DEFAULT_TAG, MPI_COMM_WORLD);
+		}
+	}
 }
 
+//Receive buckets for this process, bucketPtr points to start index where new values starthould be copied.
+void receiveMyBuckets(double * myBucket, long bucketPtr, int numProcs, int myrank){
+	MPI_Status status;
+	long size = -1;
+	for(int i = 0; i < numProcs; i++){
+		if(i == myrank)
+			continue;
+		MPI_Recv(&size, 1, MPI_LONG, i, DEFAULT_TAG, MPI_COMM_WORLD, &status);
+		if(size > 0){
+			// if(myrank == 0){
+			// 	cout << "size received: " << size << " From process: " << i << endl;
+			// }
+			MPI_Recv((void *)(myBucket + bucketPtr), size, MPI_DOUBLE, i, DEFAULT_TAG, MPI_COMM_WORLD, &status);	
+			bucketPtr += size;
+		}
+	}
+}
+
+void sendSortedBucketToMaster(double * myBucket, long bucketSize){
+
+	if(bucketSize > 0){
+		MPI_Send((void *)&bucketSize, 1, MPI_LONG, MASTER_RANK, DEFAULT_TAG, MPI_COMM_WORLD);	
+		MPI_Send((void *)myBucket, bucketSize, MPI_DOUBLE, MASTER_RANK, DEFAULT_TAG, MPI_COMM_WORLD);
+	}
+	else{
+		long negative_size = -1;
+		MPI_Send((void *)&negative_size, 1, MPI_LONG, MASTER_RANK, DEFAULT_TAG, MPI_COMM_WORLD);
+	}
+}
+
+void createSortedArr(double * x, long n, double * myBucket, long myBucketSize, int numProcs){
+	MPI_Status status;
+	long i;
+	int j;
+	for(i = 0; i < myBucketSize; i++){
+		x[i] = myBucket[i];
+	}
+
+	for(j=1; j < numProcs; j++){
+		long size=0;
+		MPI_Recv((void *)&size, 1, MPI_LONG, j, DEFAULT_TAG, MPI_COMM_WORLD, &status);
+		if(size > 0){
+			MPI_Recv((void *)(x + i), size, MPI_DOUBLE, j, DEFAULT_TAG, MPI_COMM_WORLD, &status);
+			i += size;
+		}
+	}
+}
 
 
 int main(int argc, char* argv[]){
@@ -180,11 +251,16 @@ int main(int argc, char* argv[]){
 	MPI_Comm_size(MPI_COMM_WORLD, &numProcs); 
 		
 	if(myrank == MASTER_RANK){
-		long n = 16; 
+		long n = 128; 
 		double * x = generate_array(n);
+
+		cout << "Input array: " << endl;
 		print_array(x, 0, n-1);
 		
 		long size_per_proc = n / (long)numProcs;	
+
+		//Send value of n to last node
+		MPI_Send(&n, 1, MPI_LONG, numProcs-1, DEFAULT_TAG, MPI_COMM_WORLD);
 
 		//Scatter keys to SLAVES.
 		scatterKeysToNodes(x, n, numProcs);
@@ -196,16 +272,46 @@ int main(int argc, char* argv[]){
 		double * allPivots = new double[n/q];
 		getLocalPivotsFromSlaves(allPivots, x, n, q, numProcs);
 		
-		long totalNumPivots = n/q;
+		long numLocalPivots = n/q;
 		//Sort pivots
-		insertion_sort(allPivots, 0, totalNumPivots-1);
+		insertion_sort(allPivots, 0, numLocalPivots-1);
 		
-		//Distribute global pivots.	
-		distributeGlobalPivotsToSlaves(allPivots, numProcs, totalNumPivots);
+		long numGlobalPivots = numProcs-1;
+		double * globalPivots = new double[numGlobalPivots];
+		initializeGlobalPivots(allPivots, numLocalPivots, globalPivots, numGlobalPivots);
 
+		//Distribute global pivots.	
+		distributeGlobalPivotsToSlaves(globalPivots, numGlobalPivots, numProcs);
+
+		
+		long myBucketSize = globalPivots[myrank];
+		double * myBucket = new double[myBucketSize];
+		long bucketPtr = 0;
+
+		//Send buckets to appropriate nodes.
+		long numKeys = size_per_proc; // First size_per_proc keys in array x belong to master.
+		sendBucketsToNodes(x, numKeys, globalPivots, numProcs, myrank, myBucket, &bucketPtr);//CORRECT THIS.
+
+		//Receive my buckets from other processes
+		receiveMyBuckets(myBucket, bucketPtr, numProcs, myrank);
+
+
+		//Sort my bucket
+		insertion_sort(myBucket, 0, myBucketSize-1); //NOTE: Can be optimized by putting in createSortedArr function.
+
+		//Receive final sorted buckets and create final array
+		createSortedArr(x, n, myBucket, myBucketSize, numProcs);
+
+		cout << "Sorted array: " << endl;
+		print_array(x, 0 , n-1);
 	}
 	else{//**************************************SLAVE PROCESSES************************************************
-		long dataSize, i;
+		long n, dataSize, i;
+
+		//Get value of n
+		if(myrank == numProcs-1)
+			MPI_Recv(&n, 1, MPI_LONG, MASTER_RANK, DEFAULT_TAG, MPI_COMM_WORLD, &status);
+		
 		//Get size of data to be received.
 		MPI_Recv(&dataSize, 1, MPI_LONG, MASTER_RANK, DEFAULT_TAG, MPI_COMM_WORLD, &status);
 		double * localKeys = new double[dataSize];
@@ -224,13 +330,6 @@ int main(int argc, char* argv[]){
 			tempIndex += q;
 		}
 
-		// if(myrank == 1){
-		// 	cout << "Equidistant points arr: " << endl;
-		// 	for(i=0; i < dataSize/q; i++)
-		// 		cout << arr[i] << " ";
-		// 	cout << endl;
-		// }
-
 		//Send equidistant pivots to MASTER
 		MPI_Send((void *)arr, dataSize/q, MPI_DOUBLE, MASTER_RANK, DEFAULT_TAG, MPI_COMM_WORLD);
 		
@@ -238,12 +337,24 @@ int main(int argc, char* argv[]){
 		double * globalPivArray = new double[numProcs - 1];
 		MPI_Recv(globalPivArray, numProcs-1, MPI_DOUBLE, MASTER_RANK, DEFAULT_TAG, MPI_COMM_WORLD, &status);
 		
-		//Distribute buckets to appropriate nodes
-		sendBucketsToNodes(localKeys, dataSize, globalPivArray, numProcs, myrank);
 
+		long myBucketSize = myrank != (numProcs-1) ? globalPivArray[myrank] - globalPivArray[myrank-1] : n - globalPivArray[myrank-1];
+
+		double * myBucket = new double[myBucketSize];
+		long bucketPtr = 0;
+
+		//Distribute buckets to appropriate nodes
+		sendBucketsToNodes(localKeys, dataSize, globalPivArray, numProcs, myrank, myBucket, &bucketPtr);
+
+		//Receive my buckets from other processes
+		receiveMyBuckets(myBucket, bucketPtr, numProcs, myrank);
+
+		//Sort my bucket locally
+		insertion_sort(myBucket, 0, myBucketSize-1);
+
+		//Send my bucket to MASTER
+		sendSortedBucketToMaster(myBucket, myBucketSize);
 	}
-	
-	//cout << "Rank: " << myrank << endl;
 	MPI_Finalize();
 	return 0;
 }
